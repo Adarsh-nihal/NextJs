333bb993b45923f502ece964858f199f60f74a93
Adarsh-nihal <adarsh474747@gmail.com>
2023-04-30 10:06:14 +0530

    added

--- Project/first-nextapp/pages/_app.js
+++ Project/first-nextapp/pages/_app.js
@@ -1,4 +1,7 @@
 import '@/styles/globals.css'
+import "../node_modules/react-bootstrap/dist/react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
