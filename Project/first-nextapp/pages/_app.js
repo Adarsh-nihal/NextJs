8d8f9dde785651bb0feaf52bbb242689eb994a97
Adarsh-nihal <adarsh474747@gmail.com>
2023-04-30 10:42:39 +0530

    added

--- Project/first-nextapp/pages/_app.js
+++ Project/first-nextapp/pages/_app.js
@@ -1,7 +1,6 @@
 import '@/styles/globals.css'
-import "../node_modules/react-bootstrap/dist/react-bootstrap"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
+// import "../node_modules/react-bootstrap/dist/react-bootstrap"
+// import "../node_modules/bootstrap/dist/css/bootstrap.css"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- Project/first-nextapp/pages/index.js
+++ Project/first-nextapp/pages/index.js
@@ -2,27 +2,17 @@
 import Navbar from '@/components/Navbar'
 import Link from 'next/link'
 import Head from 'next/head'
-import Button from 'react-bootstrap/Button';
+
 const index = () => {
   return (
    <div >
     <Head>
       <title>Home Page</title> 
+      
       </Head>
-   {/* <Navbar /> */}
+   <Navbar />
 <div >
-   {/* <h2>I am main page</h2> */}
-
-   <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
-
+   <h2>I am main page</h2>
    </div>
    </div>
   )
