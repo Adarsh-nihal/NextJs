cd04ad8aa98c925355525d200dcb9a6449aefe83
Adarsh Gupta <107548299+Adarsh-nihal@users.noreply.github.com>
2023-05-16 11:33:17 +0530

    Update 404.js

--- Project/first-nextapp/pages/404.js
+++ Project/first-nextapp/pages/404.js
@@ -16,7 +16,7 @@ router.push("/")
       <Navbar/>
 <h2>We are sorry this page is not found</h2>
 <Link href="/">go back to home</Link>
-<h2 onClick={()=>router.push("/")} >go by btn back to home</h2>
+<h2 onClick={()=>router.push("/")} >go by click btn back to home</h2>
 
     </div>
   )
